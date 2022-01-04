4df38ae1e144126470ec6583fb6d1ebc20d66cff
Leonardo Silva <leonardoaraujosilva3@gmail.com>
2022-01-04 19:28:28 -0300

    first commit

---
+++ src/main/java/com/lulima/ipizza/IpizzaApplication.java
@@ -0,0 +1,13 @@
+package com.lulima.ipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lulima/ipizza/IpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lulima.ipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
